717e8b3a04dc0c3a6bc16ef7435b43caac04ed24
Nathan Griffith <nategri@gmail.com>
2018-09-27 21:26:56 -0700

    will probably be revision alpha-1

--- step_up_component_values.py
+++ step_up_component_values.py
@@ -1,6 +1,22 @@
 # Reference:
 # http://www.learningaboutelectronics.com/Articles/Switch-mode-power-supply-circuit-with-an-MC34063.php
 
+# Bench test
+#
+# 3.7 V in
+# 5.37 V out
+#
+# Actual values used
+# 180 Ohm -> 220 Ohm
+# Rsc -> 0 Ohm (short)
+# 100 uF -> 100 uF
+# R1 -> 100 Ohms
+# R2 -> 330 Ohms
+# CO -> 680 uF
+# CT -> 150 pF
+# L -> 1.5 uH
+
+
 #
 # Inputs
 #
@@ -14,10 +30,10 @@ R1 = 1.0 # [ohms]
 
 f = 100.0e3 # Frequncy of circuit [Hz]
 
-V_drop_diode = 0.4 # [V]
+V_drop_diode = 0.55 # [V]
 V_sat = 0.45 # Saturation voltage of transistor in IC [V]
 
-V_ripple = 0.25
+V_ripple = 0.1
 
 #
 # Calculations
@@ -36,7 +52,7 @@ I_peak = 2 * I_out * (T_on_over_T_off + 1) # [A]
 
 R_sc = (0.3 / I_peak) / 1.0e-3 # [mOhms]
 
-L_min = (T_on * (V_in - V_sat) / I_peak) / 1.0e-6 # [uH]
+L_min = (T_on * (V_in - V_sat) / I_peak) # [uH]
 
 C_o = (9 * (I_out * T_on) / V_ripple) # [uF]
 
@@ -55,3 +71,5 @@ print("R_sc: {} mOhms".format(R_sc))
 print("")
 print("C_T: {} pF".format(C_T))
 print("C_o: {} uF".format(C_o))
+print("")
+print("L: {} uH".format(L_min))
